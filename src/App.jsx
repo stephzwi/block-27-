592894b1a3acc0353de80ef7e9a420f3b99f9777
Steph <stephanwilson10@yahoo.com>
2023-08-03 07:15:57 -0400

    updated signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import Authenticate from "./components/Authenitcate";
+import SignUpForm from "./components/SignUpForm";
+import "./App.css";
 
 function App() {
-  
+  const [token, setToken] = useState(null);
 
-  return ()
-    
+  return (
+    <>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
+    </>
+  );
 }
 
-export default App
+export default App;
